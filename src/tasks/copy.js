7357bb5332858d26c17bf941dba2c3801331944e
andrey-hohlov <andr.hohlov@gmail.com>
2017-01-23 16:00:07 +0700

    rewrite copy task

--- src/tasks/copy.js
+++ src/tasks/copy.js
@@ -1,15 +1,20 @@
 import plumber from 'gulp-plumber';
+import mergeStream from 'merge-stream';
 
 const config = protoss.config.copy;
 
-protoss.gulp.task('protoss/copy', function(cb) {
-  var files = config || [];
-  if (files.length != 0) {
-    for (var i = 0; i < files.length; i++) {
-      protoss.gulp.src(files[i][0])
-        .pipe(plumber({errorHandler: protoss.errorHandler(`Error in \'copy\' task`)}))
-        .pipe(protoss.gulp.dest(files[i][1]))
-    }
-    cb(null);
-  }
+protoss.gulp.task('protoss/copy', () => {
+  const streams = mergeStream();
+  const copy = config || [];
+
+  copy.forEach((files) => {
+    const copyStream = protoss.gulp.src(files.src)
+      .pipe(plumber({
+        errorHandler: protoss.errorHandler('Error in copy task'),
+      }))
+      .pipe(protoss.gulp.dest(files.dest));
+    streams.add(copyStream);
+  });
+
+  return streams;
 });
