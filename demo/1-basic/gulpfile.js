93d2eec6487516b75d14283330a856ccdf7eb58e
andrey-hohlov <andr.hohlov@gmail.com>
2017-01-24 14:20:30 +0700

    rm demo

--- demo/1-basic/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-/**
- * Use Protoss tasks
- */
-
-require('/Users/Andrey/Work/Repositories/protoss')(gulp, require('./protoss-config.js'));
-//require('protoss')(gulp, require('./protoss-config.js'));
